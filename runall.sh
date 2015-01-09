7880dc30a8b7523e336921f3a9362423b2b63841
Daniel Chen <Maveri2k@yahoo.com>
2015-01-09 15:46:03 -0500

    added toc option, and bash rm the .md file

--- runall.sh
+++ runall.sh
@@ -1,3 +1,6 @@
 wc pdb/*.pdb | sort -n | head -n 10 > shell_output.txt
 
-Rscript -e "library(knitr); knit2html('batch.Rmd')"
+Rscript -e "library(knitr); knit2html('batch.Rmd', options = 'toc')"
+
+echo 'delete .md file'
+rm batch.md
